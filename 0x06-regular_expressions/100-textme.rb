af1f22dfea5765fe0c3a65819abcf42beaf08318
CzarProCoder <juliusczar36@gmail.com>
2023-05-08 13:54:05 -0700

    Created a Ruby Script to output: [SENDER],[RECEIVER],[FLAGS];

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Script to output [sender],[receiver],[flags] from Logfile
 
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
