d118ecf3b3bd0e33b327ec3916380f619692de76
CzarProCoder <juliusczar36@gmail.com>
2023-07-24 08:30:55 -0700

    Made the file executable

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@
 Python script that, using this REST API, for a given employee ID,
     returns information about his/her TODO list progress
 '''
-import request
+import requests
 from sys import argv
 
 
