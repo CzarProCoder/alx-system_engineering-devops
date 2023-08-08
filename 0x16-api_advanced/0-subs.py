7d907a217ed0da746f41e44e33aa13b68afe81d4
CzarProCoder <juliusczar36@gmail.com>
2023-08-08 21:17:10 +0300

    Add script that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,14 +11,11 @@ def number_of_subscribers(subreddit):
     Return the total number of subscribers on a given subreddit
     '''
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    print(url)
 
     user_agent = 'Mozilla/5.0'
     headers = {'User-Agent': user_agent}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    print(response.status_code)
 
     if response.status_code != 200:
         return 0
